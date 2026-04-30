6513e95663ba1e8ac6f3fe044d3f2e486af42216
Aparecida <aparecidalml0@gmail.com>
2026-04-29 21:11:43 -0300

    rota post

--- servidor_http/server.js
+++ servidor_http/server.js
@@ -1,14 +1,14 @@
-import http from 'http'
-import fs from 'fs'
-import queryString from 'querystring'
-import { URLSearchParams } from 'url'
+import http from 'http' 
+import fs from 'fs' 
+import queryString from 'querystring' // pega a parte final da URL da requisição (após o símbolo ?)
 
 const PORTA = 3000
 const HOST = 'localhost'
+let cursos = [] // array para armazenar os cursos cadastrados
 
 const server = http.createServer((req, res) => {
-    // const {url, method} = req //    const url = req.url    const method = req.method
-    console.log(req.url, ' - ', req.method)// conferir a requisição - URL e Método
+    // const {url, method} = req //    desestruturação do objeto, o mesmo que: const url = req.url    const method = req.method
+    console.log(`Requisição: URL - ${req.url} - Método: ${req.method}`) // conferir a requisição - URL e Método
     if(req.url === '/' && req.method === 'GET'){
         res.writeHead(200, {"content-type": 'text/html; charset=utf-8'})
         // res.write()
@@ -16,28 +16,42 @@ const server = http.createServer((req, res) => {
     }else if(req.url === '/cadastro' && req.method === 'GET'){
          res.writeHead(200, {"content-type": 'text/html; charset=utf-8'})   
         res.end(fs.readFileSync('cadastro.html', 'utf-8'))
-    } else if(req.url === '/cursos' && req.method === 'POST'){
-        res.writeHead(200, {"content-type": 'text/html; charset=utf-8'})     
+    } else if(req.url === '/curso' && req.method === 'POST'){
+        res.writeHead(200, {"content-type": 'text/html; charset=utf-8'}) //resposta - página HTML
+        // res.writeHead(200, {"content-type": 'aplication/json; charset=utf-8'}) //resposta - JSON
         let dados = ''
         req.on('data', chunk => {
-            dados += chunk.toString()
+            dados += chunk.toString('utf-8')
         })
         req.on('end', () => {
-            const dados_req = queryString.parse(dados) 
-            const dados_req1 = URLSearchParams(dados)
-            res.write('<h1> Cursos </h1>')          
-            res.end(`<h3> Curso: ${dados_req.curso}  </h3>`)
+            const dados_req1 = queryString.parse(dados) //curso=DS&ch=1200&tipo=tecnico = {curso: 'DS', ch: '1200', tipo: 'tecnico'}
+            const dados_req2 = new URLSearchParams(dados) //converte a string da requisição em objeto javascript
+            cursos.push(dados_req2) // adiciona o curso cadastrado no array de cursos            
+            // res.write('<h1> Cursos </h1>')          
+            // res.end(`
+            //     <h3> Curso: ${dados_req1.curso} <br> 
+            //     Carga Horária: ${dados_req1.ch} <br> 
+            //     Tipo: ${dados_req2.get('tipo')} </h3>`)// resposta em HTML
+            res.end(JSON.stringify({
+                curso: dados_req1.curso, 
+                ch: dados_req1.ch, 
+                tipo: dados_req2.get('tipo')})) // resposta em JSON
+           
         })
-        
 
-    }
-    else{
+    }else if(req.url === '/cursos' && req.method === 'GET'){
+         res.writeHead(200, {"content-type": 'text/html; charset=utf-8'})
+         res.end(`
+            <h1> Lista de Cursos </h1>
+            <h3> ${cursos.map(curso => `
+                <br> Curso: ${curso.get('curso')} <br>
+                Carga Horária: ${curso.get('ch')} <br>
+                Tipo: ${curso.get('tipo')} <br> `
+            )} </h3> `) // acessa o array com os cursos e mostra as informações no HTML
+    }else{
         res.writeHead(404, {"content-type": 'text/html; charset=utf-8'})
         res.end('<h1> Página não localizada! </h1>')
     }
-
-
-    
 
 })
 
